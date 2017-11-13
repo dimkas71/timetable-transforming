461130a9d902329043ab93da2eef7a918c448446
Dimkas 71 <dimkas71g@gmail.com>
2017-11-13 18:20:52 +0200

    added merging two files in an one

--- src/main/java/ua/compservice/TimeTableExcelApplication.java
+++ src/main/java/ua/compservice/TimeTableExcelApplication.java
@@ -1,13 +1,9 @@
 package ua.compservice;
 
-import ua.compservice.model.ImmutableCell;
 import ua.compservice.util.WorkbookUtils;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TimeTableExcelApplication {
 
@@ -17,58 +13,17 @@ public class TimeTableExcelApplication {
 
 
     public static void main(String[] args) {
-        Path source = Paths.get(System.getProperty("user.dir").toString() + "/src/files/11.xlsx");
+        Path currentDir = Paths.get(System.getProperty("user.dir").toString()).resolve("src/files");
 
-        if (Files.exists(source)) {
-            List<ImmutableCell> content = WorkbookUtils.from(source);
+        Path file1 = currentDir.resolve(Paths.get("11.xlsx"));
 
+        Path file2 = currentDir.resolve(Paths.get("46.xlsx"));
 
-            //
-            Integer fioRow = content.stream()
-                    .filter(c -> c.getValue().contains(FIO_SEARCH_TEXT))
-                    .map(c -> c.getRow())
-                    .findFirst()
-                    .orElse(-1);
+        Path to = currentDir.resolve(Paths.get("common.xlsx"));
 
-            System.out.println(fioRow);
-
-            Integer tableNumberRow = content.stream()
-                    .filter(c -> c.getValue().contains(TABLE_NUMBER_SEARCH_TEXT))
-                    .map(c -> c.getRow())
-                    .findFirst()
-                    .orElse(-1);
-
-            Integer currentPositionRow = content.stream()
-                    .filter(c -> c.getValue().contains(CURRENT_POSITION_SEARCH_TEXT))
-                    .map(c -> c.getRow())
-                    .findFirst()
-                    .orElse(-1);
+        WorkbookUtils.merge(to, file1, file2);
 
 
-            Integer firstDayColumn =
-                    content.stream()
-                        .filter(cell -> cell.getRow() == tableNumberRow && cell.getValue().equals("1"))
-                        .map(c -> c.getColumn())
-                        .findFirst()
-                        .orElse(-1);
-
-            System.out.println(firstDayColumn);
-
-
-            List<ImmutableCell> header = content.stream()
-                    .filter(c -> c.getRow() == fioRow)
-                    .collect(Collectors.toList());
-
-
-
-
-
-
-
-
-            System.out.println(header);
-
-        }
 
     }
 }
--- src/main/java/ua/compservice/util/WorkbookUtils.java
+++ src/main/java/ua/compservice/util/WorkbookUtils.java
@@ -4,19 +4,30 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import ua.compservice.model.ImmutableCell;
+import ua.compservice.model.ImmutableRow;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class WorkbookUtils {
 
-    public static final String NO_VALUE = "UNDEFINED VALUE";
+    public static final String TABLE_NUMBER_SEARCH_TEXT = "Таб. №";
+
+    public static final String NO_VALUE = "";
 
     public static List<ImmutableCell> from(Path source) {
 
@@ -72,4 +83,80 @@ public final class WorkbookUtils {
         return content;
 
     }
+
+    public static void merge(Path to, Path... filesFrom) {
+
+
+        try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+
+            XSSFSheet currentSheet = workbook.createSheet("timetable");
+
+            int currentRow = 0;
+
+            for (Path path : filesFrom) {
+                List<ImmutableCell> content = from(path);
+
+                int tableNumberColumn = content.stream()
+                        .filter(c -> c.getValue().contains(TABLE_NUMBER_SEARCH_TEXT))
+                        .map(c -> c.getRow())
+                        .findFirst()
+                        .orElse(-1);
+
+                if (tableNumberColumn != -1) {
+
+                    int maxRow = content.stream()
+                            .map(c -> c.getRow())
+                            .max((r1, r2) -> (r1 > r2) ? 1 : (r1 < r2) ? -1 : 0)
+                            .orElse(-1);
+
+                    final int from = content.stream()
+                            .filter(c -> Utils.matches(c.getValue()) && (c.getColumn() == tableNumberColumn))
+                            .map(c -> c.getRow())
+                            .findFirst()
+                            .orElse(-1);
+
+                    final int currentRow1 = currentRow;
+                    content.stream()
+                            .filter(c -> c.getRow() >= from)
+                            .forEach(c -> {
+
+                                int rowNum = currentRow1 + c.getRow();
+
+                                XSSFRow row = currentSheet.getRow(rowNum);
+                                if (row == null) {
+                                    row = currentSheet.createRow(rowNum);
+                                }
+
+                                XSSFCell cell = row.getCell(c.getColumn());
+                                if (cell == null) {
+                                    cell = row.createCell(c.getColumn(), CellType.STRING);
+                                }
+
+                                cell.setCellValue(c.getValue());
+                            });
+
+                    currentRow += maxRow + 1;
+
+                }
+
+            }
+
+            try (FileOutputStream fos = new FileOutputStream(to.toFile())) {
+                workbook.write(fos);
+            }
+
+            workbook.close();
+
+        } catch (IOException e) {
+
+        }
+
+
+
+
+
+
+    }
 }
+
+
